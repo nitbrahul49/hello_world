2be71e5801865aec9bcd354fc1f035eab019aec6
nitbrahul49 <42375334+nitbrahul49@users.noreply.github.com>
2018-08-31 17:23:00 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,11 @@
-public class Hello{
-     public static void main(String[] args)
-     {
-     	for(int i =0;i<10;i++)
-     {
- 	System.out.pringln("Hello world changed");
-    }
-   }
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for(int i =0;i<10;i++)
+			{
+				System.out.println("hello");
+			}
+	}
+
 }
